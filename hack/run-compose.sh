1977f64f35354730fa71b95b778645e2af66f6e6
Mert Bicak <mbicak@redhat.com>
2025-11-11 10:25:38 +0100

    fix: make compose linux compatible, fix issues in property config

--- hack/run-compose.sh
+++ hack/run-compose.sh
@@ -16,10 +16,11 @@ bash ./hack/build-with-schemas.sh
 echo "--- Switching to podman folder ---"
 pushd podman
 
-echo "--- Stopping existing podman-compose ---"
-podman-compose down
+echo "--- Creating bindmounts --"
+[ ! -d "./kafka-data" ] && mkdir ./kafka-data && podman unshare chown 1001:0 ./kafka-data
+[ ! -d "./kafka-config" ] && mkdir ./kafka-config && podman unshare chown 1001:0 ./kafka-config
 
 echo "--- Starting podman-compose ---"
-podman-compose up --build
+podman-compose up --build --force-recreate
 
 echo "--- Local podman-compose is now running ---"
